297d54b4bd4cc6c006857d22fa368e19f3c51d69
Memo Martell <gucasta@me.com>
2024-02-03 20:24:59 -0600

    End of section 13

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -2,7 +2,7 @@
 
 import { FieldValues } from "react-hook-form";
 import { fetchWrapper } from "../lib/fetchWrapper";
-import { Auction, PagedResult } from "../types";
+import { Auction, Bid, PagedResult } from "../types";
 import { revalidatePath } from "next/cache";
 
 export async function getData(query: string): Promise<PagedResult<Auction>> {
@@ -33,4 +33,12 @@ export async function updateAuction(data: FieldValues, id: string) {
 
 export async function deleteAuction(id: string) {
     return await fetchWrapper.del(`auctions/${id}`);
+}
+
+export async function getBidsForAuction(id: string): Promise<Bid[]> {
+    return await fetchWrapper.get(`bids/${id}`);
+}
+
+export async function placeBidForAuction(auctionId: string, amount: number) {
+    return await fetchWrapper.post(`bids?auctionId=${auctionId}&amount=${amount}`, {})
 }
