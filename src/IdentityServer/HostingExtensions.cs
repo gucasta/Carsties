b647d0e304c8b28200662f6a7bf846d642c8a42c
Memo Martell <gucasta@me.com>
2024-02-04 22:39:10 -0600

    End of Appendix B

--- src/IdentityServer/HostingExtensions.cs
+++ src/IdentityServer/HostingExtensions.cs
@@ -1,4 +1,5 @@
 using Duende.IdentityServer;
+using Duende.IdentityServer.Services;
 using IdentityServer;
 using ItentityServer.Data;
 using ItentityServer.Models;
@@ -64,6 +65,16 @@ internal static class HostingExtensions
 
         app.UseStaticFiles();
         app.UseRouting();
+        
+        if (app.Environment.IsProduction()) 
+        {
+            app.Use(async (ctx, next) => {
+                var serverUrls = ctx.RequestServices.GetRequiredService<IServerUrls>();
+                serverUrls.Origin = serverUrls.Origin = "https://id.martelldevel.online";
+                await next();
+            });
+        }
+
         app.UseIdentityServer();
         app.UseAuthorization();
         
